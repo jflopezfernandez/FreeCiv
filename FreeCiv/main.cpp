98a8e92c53976bd369294958b39637eb22bce857
lopezfjose <jflopezfernandez@gmail.com>
2018-04-28 17:51:50 -0400

    Adding documentation configuration and tag files

--- FreeCiv/main.cpp
+++ FreeCiv/main.cpp
@@ -1,8 +1,58 @@
 
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
+
+/** Function:			PrintGLFWVersion()
+ *
+ *  Description:		This function is inline because 'glfwGetVersionString()' must be
+ *                      called from the main thread.
+ *
+ *  Return Type:		void
+ */
+
+inline void PrintGLFWVersion() {
+	std::cout << "GLFW Version: " << glfwGetVersionString() << '\n';
+}
+
 
 int main()
 {
-	std::cout << "testing...\n";
+	//std::cout << "GLFW Version: " << glfwGetVersionString() << '\n';
+	PrintGLFWVersion();
+
+	const auto NullVar = GLFW_NOT_INITIALIZED;
+
+	GLFWwindow *window;
+
+	// Initialize the library
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	// Create a windowed mode window and its OpenGL context
+	window = glfwCreateWindow(640, 480, "Hello, World!", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	// Loop until the user closes the window
+	while (!glfwWindowShouldClose(window)) {
+		// Render here
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+
+		// Poll for and process events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
